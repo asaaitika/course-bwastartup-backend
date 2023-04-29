a7cd0f8166d92905582a989a73d390bd41dba780
Permata Asa Aitika <asaaitika@gmail.com>
2023-04-29 23:47:42 +0700

    add register user repository

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"course-bwastartup-backend/user"
-	"fmt"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
@@ -24,37 +21,10 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connect to Database:D")
-
-	var users []user.User
-	db.Find(&users)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// 	fmt.Println("===================")
-	// }
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-
-}
-
-func handler(c *gin.Context) {
-	dsn := "root:F!rentia2818@tcp(localhost:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true,
-		},
-	})
-
-	if err != nil {
-		log.Fatal(err.Error())
+	userRepository := user.NewRepository(db)
+	user := user.User{
+		Name: "test simpan",
 	}
 
-	var users []user.User
-	db.Find(&users)
-
-	c.JSON(http.StatusOK, users)
+	userRepository.Save(user)
 }
