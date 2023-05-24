445db4b248db0463fd6731edcc46671d6a532f4a
Permata Asa Aitika <asaaitika@gmail.com>
2023-05-24 23:28:22 +0700

    add endpoint campaigns -on going

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,5 +1,36 @@
 package handler
 
+import (
+	"course-bwastartup-backend/campaign"
+	"course-bwastartup-backend/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
 /*1. tangkap parameter di handler
 2. handler ke service, di service menentukan apakah repository mana yang dicall
 3. repository akses ke db: GetAll (FindAll), GetByUserID(FinfByUserID)*/
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	userId, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userId)
+	if err != nil {
+		response := helper.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("List of campaigns", http.StatusOK, "success", campaigns)
+	c.JSON(http.StatusOK, response)
+}
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"course-bwastartup-backend/handler"
 	"course-bwastartup-backend/helper"
 	"course-bwastartup-backend/user"
-	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -37,10 +36,8 @@ func main() {
 	campaignService := campaign.NewService(campaignRepository)
 	authService := auth.NewService()
 
-	campaigns, _ := campaignService.FindCampaigns(3)
-	fmt.Println(len(campaigns))
-
 	userHandler := handler.NewUserHandler(userService, authService)
+	campaignHandler := handler.NewCampaignHandler(campaignService)
 
 	router := gin.Default()
 	api := router.Group("/api/v1")
@@ -49,6 +46,8 @@ func main() {
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
+
+	api.GET("/campaigns", campaignHandler.GetCampaigns)
 
 	router.Run()
 }
