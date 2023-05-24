9046b65734acab21073ca16617b6b84a63b6cbad
Permata Asa Aitika <asaaitika@gmail.com>
2023-05-25 00:00:37 +0700

    endpoint campaign images done

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,39 @@
 package campaign
+
+type CampaignFormatter struct {
+	Id               int    `json:"id"`
+	UserId           int    `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageUrl         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	campaignFormatter := CampaignFormatter{}
+	campaignFormatter.Id = campaign.ID
+	campaignFormatter.UserId = campaign.UserID
+	campaignFormatter.Name = campaign.Name
+	campaignFormatter.ShortDescription = campaign.ShortDescription
+	campaignFormatter.GoalAmount = campaign.GoalAmout
+	campaignFormatter.CurrentAmount = campaign.CurrentAmout
+	campaignFormatter.ImageUrl = ""
+
+	if len(campaign.CampaignImages) > 0 {
+		campaignFormatter.ImageUrl = campaign.CampaignImages[0].FileName
+	}
+
+	return campaignFormatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	campaignsFormatter := []CampaignFormatter{}
+
+	for _, campaign := range campaigns {
+		campaignFormatter := FormatCampaign(campaign)
+		campaignsFormatter = append(campaignsFormatter, campaignFormatter)
+	}
+
+	return campaignsFormatter
+}
